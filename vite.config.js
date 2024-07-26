0aeab025bbc988cfa2a9b03a1e9a53f4791a8eb7
mukumbasar <mehmetutkununoyunlari@gmail.com>
2024-07-26 10:16:08 +0300

    Prepare for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/ptn-demo-ui/',
   plugins: [react()],
+  base: '/ptn-demo-ui/',
 })
