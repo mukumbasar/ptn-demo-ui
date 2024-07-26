c588f1c626bd4b17ada17fa699e80dfc52b82486
mukumbasar <mehmetutkununoyunlari@gmail.com>
2024-07-26 09:49:06 +0300

    Complete for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ptn-demo-project-react/',
   plugins: [react()],
+  build: {
+    outDir: 'build'
+  },
+  base: '/ptn-demo-ui/'
 })
