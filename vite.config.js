0f2f1a155b85e0e3437d9a091dbdad2a2794e44a
mukumbasar <mehmetutkununoyunlari@gmail.com>
2024-07-26 09:28:02 +0300

    Publish

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ptn-demo-project-ui/',
+  base: '/ptn-demo-project-react/',
   plugins: [react()],
 })
